41e482462c1973aeb9a71fb090376be41e038ee4
Hrishikesh Kumar <hkumar@infinera.com>
2020-02-13 14:30:29 +0530

    path

--- fileCompactor.js
+++ fileCompactor.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const es = require('event-stream');
 const Utils = require ('../xtend-libs/utils');
+const path = require('path');
+const LOG_DIR = path.resolve(__dirname);
 
 var fileCompacter = async function () {
+    console.log(LOG_DIR);
     var p = await Utils.execCmdExecutor ('dir' , Infinity);
     console.log('[output] = ', p);
 }
